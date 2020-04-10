05a4a8807df2cfc364d83d43da66cab506b17653
Smugatkar <48935757+Smugatkar@users.noreply.github.com>
2020-04-10 17:33:18 +0530

    Add files via upload

---
+++ BasicDataTypes.java
@@ -0,0 +1,23 @@
+package CoreJavBasics;
+
+public class BasicDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i =10;
+		float f = 1;
+		 double d=100.23;
+		 char c='f';
+		 String s="Shailesh";
+		 
+		 System.out.println("please find following data types:");
+		 System.out.println("integer: "+i);
+		 System.out.println("float: "+f);
+		 System.out.println("double: "+d);
+		 System.out.println("char: "+c);
+		 System.out.println("Sting: "+s);
+		
+	}
+
+}
